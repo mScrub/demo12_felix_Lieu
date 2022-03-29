b80a3eab9c41787eebcd117d7fa298d63f8a8b95
Felix <flieu@my.bcit.ca>
2022-03-29 10:24:50 -0700

    added images and different styling

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,14 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    // File stays on computer
+    apiKey: "AIzaSyBDNZUPQCgl6uLnRoXrYcPmMtU6_37AYz0",
+    authDomain: "fir-comp1800-d8111.firebaseapp.com",
+    projectId: "fir-comp1800-d8111",
+    storageBucket: "fir-comp1800-d8111.appspot.com",
+    messagingSenderId: "930855711186",
+    appId: "1:930855711186:web:5ba1c0a3785aaa1b9ad80b",
+    measurementId: "G-45HM5W376Y"
 };
 
 //--------------------------------------------
